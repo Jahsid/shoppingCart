9937fd29c0a7b9101a9382cff0e3828beeecdeea
jahsid <muhammedjahsid@gmail.com>
2022-01-14 04:25:28 +0530

    edit product

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -1,6 +1,7 @@
 var db=require('../config/connection')
 var collection=require('../config/collections')
 var objectId=require('mongodb').ObjectID
+const { response } = require('../app')
 module.exports={
     addProduct:(product,callback)=>{
         
@@ -22,6 +23,27 @@ module.exports={
             db.get().collection(collection.PRODUCT_COLLECTION).removeOne({_id:objectId(proId)}).then((response)=>{
                 //console.log(response);
                 resolve(response)
+            })
+        })
+    },
+    getProductDetails:(proId)=>{
+        return new Promise((resolve,reject)=>{
+            db.get().collection(collection.PRODUCT_COLLECTION).findOne({_id:objectId(proId)}).then((product)=>{
+                resolve(product)
+            })
+        })
+    },
+    updateProduct:(proId,proDetails)=>{
+        return new Promise((resolve,reject)=>{
+            db.get().collection(collection.PRODUCT_COLLECTION).updateOne({_id:objectId(proId)},{
+                $set:{
+                    Name:proDetails.Name,
+                    Description:proDetails.Description,
+                    Price:proDetails.Price,
+                    Catergory:proDetails.Catergory
+                }
+            }).then((response)=>{
+                resolve()
             })
         })
     }
