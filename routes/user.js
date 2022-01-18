3fce283e136807c14d43770e03eaf43f60d5f101
jahsid <muhammedjahsid@gmail.com>
2022-01-19 03:38:56 +0530

    show cart

--- routes/user.js
+++ routes/user.js
@@ -35,6 +35,9 @@ router.get('/signup',(req,res)=>{
 router.post('/signup',(req,res)=>{
   userHelpers.doSignup(req.body).then((response)=>{
     console.log(response);
+    req.session.loggedIn=true
+    req.session.user=response.user
+    res.redirect('/')
   })
 })
 router.post('/login',(req,res)=>{
@@ -53,8 +56,14 @@ router.get('/logout',(req,res)=>{
   req.session.destroy()
   res.redirect('/')
 })
-router.get('/cart',verifyLogin,(req,res)=>{
-  res.render('user/cart')
+router.get('/cart',verifyLogin,async(req,res)=>{
+  let products=await userHelpers.getCartProducts(req.session.user._id)
+  console.log(products)
+  res.render('user/cart',{products,user:req.session.user})
 })
-
+router.get('/add-to-cart/:id',verifyLogin,(req,res)=>{
+  userHelpers.addToCart(req.params.id,req.session.user._id).then(()=>{
+    res.redirect('/')
+  })
+})
 module.exports = router;
